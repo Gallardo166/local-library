1286dcf3a5d857d80256534bd4cbc638b49f8244
Gallardo Lee <leegallardo16@gmail.com>
2024-06-02 00:11:40 +0700

    Render details for list and detail pages

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -2,11 +2,27 @@ const BookInstance = require("../models/bookinstance");
 const asyncHandler = require("express-async-handler");
 
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
+  const allBookInstances = await BookInstance.find().populate("book").exec();
 
+  res.render("bookinstance_list", { 
+    title: "Book Instance List",
+    bookinstance_list: allBookInstances,
+  })
 });
 
 exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
+  const bookInstance = await BookInstance.findById(req.params.id).populate("book").exec();
 
+  if (bookInstance === null) {
+    const err = new Error("Book copy not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bookinstance_detail", {
+    title: "Book:",
+    bookinstance: bookInstance,
+  })
 });
 
 exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -24,7 +25,7 @@ const AuthorSchema = new Schema({
 AuthorSchema.virtual("name").get(function () {
   let fullname = "";
   if (this.first_name && this.family_name) {
-    fullname = this.first_name + this.family_name;
+    fullname = this.first_name + ', ' + this.family_name;
   }
 
   return fullname;
@@ -33,5 +34,11 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
+
+AuthorSchema.virtual("years").get(function () {
+  return (this.date_of_birth && this.date_of_death)
+    ? `(${DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)} - ${DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)})`
+    : '';
+})
 
 module.exports = mongoose.model("Author", AuthorSchema);
